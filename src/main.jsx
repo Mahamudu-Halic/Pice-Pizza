dc166825272fd2ffca5ae1a4ed00765690244db1
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-05-24 12:33:57 +0000

    clerk

--- src/main.jsx
+++ src/main.jsx
@@ -1,48 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
-import {
-  ClerkProvider,
-  SignedIn,
-  SignedOut,
-  SignIn,
-  SignUp,
-  RedirectToSignIn,
-} from "@clerk/clerk-react";
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
-import LandingPage from "./screens/landing-page.screen.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { ClerkProviderWithRoutes } from "./auth.js";
 
-const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-if (!clerkPubKey) {
-  throw new Error("Missing Publishable Key");
-}
 
-function ClerkProviderWithRoutes() {
-  const navigate = useNavigate();
 
-  return (
-    <ClerkProvider publishableKey={clerkPubKey} navigate={(to) => navigate(to)}>
-      <SignedIn>
-        <App />
-      </SignedIn>
-      <SignedOut>
-        <RedirectToSignIn />
-      </SignedOut>
-      <Routes>
-        <Route
-          path="/sign-in/*"
-          element={<SignIn routing="path" path="/sign-in" />}
-        />
-        <Route
-          path="/sign-up/*"
-          element={<SignUp routing="path" path="/sign-up" />}
-        />
-      </Routes>
-    </ClerkProvider>
-  );
-}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
