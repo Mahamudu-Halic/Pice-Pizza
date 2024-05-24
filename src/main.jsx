df0ab32a14643619b5a0284f324a9012dc78c0aa
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-05-24 12:48:43 +0000

    clerk

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { ClerkProviderWithRoutes } from "./auth.js";
-
-
-
-
+import { ClerkProviderWithRoutes } from "./auth";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
