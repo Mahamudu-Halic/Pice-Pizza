4589fe75e0eaa69b7afa1eba0af015578dabbf9b
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-05-24 13:03:40 +0000

    clerk

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ClerkProviderWithRoutes } from "./auth";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ClerkProviderWithRoutes />
-      {/* <App /> */}
+      {/* <ClerkProviderWithRoutes /> */}
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
