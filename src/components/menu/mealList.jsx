f7548238e10b8a616ab0b11a22fbac15bbe389e6
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-08-16 04:48:40 +0000

    add order items and admin form

--- src/components/menu/mealList.jsx
+++ src/components/menu/mealList.jsx
@@ -3,30 +3,25 @@ import { MealListItems, MenuListItems } from "../../constant";
 import Meal from "./meal";
 import MenuList from "./menuList";
 import { AdminContext } from "../../services/admin/admin.context";
+import { Empty } from "../empty";
+import { Loader } from "../loader";
 
 const MealList = () => {
-  const [currentMeal, setCurrentMeal] = useState("Breakfast");
-
-  const {menu} = useContext(AdminContext)
+  const { menu, isLoading } = useContext(AdminContext);
 
   return (
     <div className="mealList">
-      {/* <div className="cardList mealListContainer">
-        {MealListItems.map((meal) => (
-          <Meal
-            meal={meal}
-            key={meal.title}
-            currentMeal={currentMeal}
-            setCurrentMeal={setCurrentMeal}
-          />
-        ))}
-      </div> */}
-
-      <div className="menuList">
-        {menu.map((menu) => (
-          <MenuList menu={menu} key={menu?._id} />
-        ))}
-      </div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="menuList">
+          {menu.length > 0 ? (
+            menu.map((menu) => <MenuList menu={menu} key={menu?._id} />)
+          ) : (
+            <Empty caption="no menus found" />
+          )}
+        </div>
+      )}
     </div>
   );
 };
