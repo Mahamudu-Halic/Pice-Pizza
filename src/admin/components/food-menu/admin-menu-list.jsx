9a6b1efca3ed8900f127c3d50c0ea5da52ebbafa
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-08-28 14:00:13 +0000

    removed unused imports

--- src/admin/components/food-menu/admin-menu-list.jsx
+++ src/admin/components/food-menu/admin-menu-list.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import AdminMenuItem from "./admin-menu-item";
 import { AdminContext } from "../../../services/admin/admin.context";
 import { Empty } from "../../../components/empty";
